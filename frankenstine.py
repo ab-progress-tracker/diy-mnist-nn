2bc6230db6195e19867d43c0a2a7492ea42d3d8f
ab-progress-tracker <agambajwa270@gmail.com>
2025-01-24 19:16:55 -0500

    Add files via upload
    Split into dev&test

--- frankenstine.py
+++ frankenstine.py
@@ -7,22 +7,41 @@ data = h5py.File('MNISTdata.hdf5', 'r')
 train_img = np.float16(data['x_train'][:])
 train_lbl = np.float16(data['y_train'][:,0])
 
-# print(len(train_img))
+test_img = np.float16(data['x_test'][:])
+test_lbl = np.float16(data['y_test'][:,0])
 
-rnum=random.randint(0,9999)
+data.close()
 
-print(data['x_train'].shape) #i just wanted to clarify the format it was in bc plt was being pissy
-print(train_img[rnum]) 
-print(train_lbl[rnum])
+dev_percentage = 0.3
+
+dev_size = int(len(test_img) * dev_percentage)
+
+print(len(test_img))
+
+dev_img = test_img[:dev_size]
+dev_lbl = test_lbl[:dev_size]
+
+test_img = test_img[dev_size:]
+test_lbl = test_lbl[dev_size:]
+
+print(f"Dev set size: {len(dev_img)}, {len(dev_lbl)}")
+print(f"Actual test set size: {len(test_img)}, {len(test_lbl)}")
+
+
+# rnum=random.randint(0,9999)
+
+# print(data['x_train'].shape) #i just wanted to clarify the format it was in bc plt was being pissy
+# print(train_img[rnum]) 
+# print(train_lbl[rnum])
 
 
 
-image = train_img[rnum]  
-label = train_lbl[rnum] 
+# image = train_img[rnum]  
+# label = train_lbl[rnum] 
 
-image = image.reshape(28, 28) #figured out why plt was being pissy --- the images are 1D array of 784 pixels
+# image = image.reshape(28, 28) #figured out why plt was being pissy --- the images are 1D array of 784 pixels
 
-plt.imshow(image,cmap='binary')
-plt.title(f"Label: {label}")  
-plt.show()
+# plt.imshow(image,cmap='binary')
+# plt.title(f"Label: {label}")  
+# plt.show()
 
