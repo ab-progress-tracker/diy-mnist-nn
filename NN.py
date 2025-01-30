cbffefd66cc6236fc3f084ad0daa16b2af3069b8
ab-progress-tracker <agambajwa270@gmail.com>
2025-01-29 22:20:59 -0500

    ADDED FUNCTION UPDATING VARS ACCORDING TO GRADIENT
    I'M A GENIUS AND CAN'T BE STOPPEDDDDDDD

--- NN.py
+++ NN.py
@@ -37,11 +37,11 @@ class NeuralNetwork:
             a_h = np.maximum(0, z_h) # it can't just be max() bc it's a vector. oops
 
             z_o = np.dot(a_h, self.weights_hidden_output) + self.biases_hidden_output
-            a_o = np.exp(z_o)/np.sum(np.exp(z_o)) 
+            a_o = np.exp(z_o)/np.sum(np.exp(z_o), axis = 0) 
             return a_o
 
         def loss(self, y_pred, y_true):
-            cost = np.sum((y_pred-y_true)**2)
+            cost = np.sum((y_pred-y_true)**2, axis = 0)
             return cost
 
         def softmax_derivative(self, a_o):
@@ -52,48 +52,59 @@ class NeuralNetwork:
             for i in range(n):
                 for j in range(n):
                     if i == j:
-                        j_m[i, j] = s[i]*(1-s[j])
+                        # diagonals
+                        self.j_m[i, j] = s[i]*(1-s[i])
                     else:
-                        j_m[i, j] = s[i]*(-s[j])
-            return j_m
+                        # off-diagonal
+                        self.j_m[i, j] = s[i]*(-s[j])
 
         def derivatives(self): # also other vars but i ain't putting em in yet because i'm Like that
             d_relu = (z_h > 0) * 1
 
-            dc_w2 = a_h*j_m*2(a_o-y_true) # derivative of cost function wrt weight 2
-            # yes ik i have to do matrix multiplication here. i'll get to it
-            dc_b2 = j_m*2(a_o-y_true) # derivative of cost function with respect to bias 2
+            dc_w2 = np.dot(a_h.T, np.dot(j_m, 2*(a_o-y_true)))# derivative of cost function wrt weight 2
+            # this basically does: a_h * j_m * 2(a_o-y_true)---checks out if you wanna do the calc. i transposed a_h because it's in the wrong shape otherwise.
+            dc_b2 = np.sum(np.dot(j_m, 2*(a_o-y_true)), axis = 0) # derivative of cost function with respect to bias 2
 
-            dc_a_h = w2*a_h*j_m*2(a_o-y_true) # i'll need this later---dc wrt activation from hidden
+            dc_a_h = np.dot(w2, np.dot(j_m, 2*(a_o-y_true))) # dc wrt activation from hidden
+            #w2*a_h*j_m*2(a_o-y_true) 
 
             # derivative of cost function with respect to weight 1
+            dc_w1 = np.dot(x.T, d_relu*dc_a_h)
 
             # derivative of cost function with respect to bias 1
-            
+            dc_b1 = np.sum(d_relu*dc_a_h)
+        
+        def update_vars(self)
+            self.weights_hidden_output = -0.1*dc_w2
+            self.biases_hidden_output = -0.1*dc_b2
+            self.weights_input_hidden = -0.1*dc_w1
+            self.biases_input_hidden = -0.1*dc_b1
 
-
+        def accuraccy(self, a_o, y_true)
+            predictions = np.argmax(a_o)
+            accuraccy = np.sum(predictions == y_true) / y_true.size
 
 nn = NeuralNetwork()
 
-z=nn.forward(10)
-print(z)
+# z=nn.forward(10)
+# print(z)
 
-somenumberidk = 3
+# somenumberidk = 3
 
-for epoch in range(somenumberidk):
-    sumloss = 0
-    for i in range(len(train_img)):
+# for epoch in range(somenumberidk):
+#     sumloss = 0
+#     for i in range(len(train_img)):
         
-        x = train_img[i]
-        y = int(train_lbl[i])
+#         x = train_img[i]
+#         y = int(train_lbl[i])
 
-        y_pred = nn.forward(x)
-        y_true = nn.hot_encode(y)
+#         y_pred = nn.forward(x)
+#         y_true = nn.hot_encode(y)
 
-        loss = nn.loss(y_pred, y_true)
-        sumloss += loss
+#         loss = nn.loss(y_pred, y_true)
+#         sumloss += loss
 
-        # add backprop stuff
+#         # add backprop stuff
 
-    print(f"Epoch {epoch+1}/{somenumberidk}, cost: {sumloss/len(train_img)}")
+#     print(f"Epoch {epoch+1}/{somenumberidk}, cost: {sumloss/len(train_img)}")
 
